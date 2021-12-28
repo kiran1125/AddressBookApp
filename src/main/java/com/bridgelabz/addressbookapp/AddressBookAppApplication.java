d183884dd41e1b5a1d0cab784ff2d61b278c99aa
Kiran <makamkiran95@gmail.com>
2021-12-28 09:38:35 +0530

    Added : Setting the data base

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -12,8 +12,8 @@ public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
-		log.info("Address Book App Started in {} Environment",context.getEnvironment()
-															.getProperty("environment"));
+		log.info("Address Book App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Address Book App DB user is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
